377a413ad3ecae1c7e30bd928a429f739359eb5f
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2026-08-16 21:44:42 +0530

    update : app init

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Get Repeat",
+  description: "Get Repeat App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${poppins.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
